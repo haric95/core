847e34ed6a17689971e901e56e79e0de7119941c
Hari Chauhan <hari-c@hotmail.co.uk>
2023-05-29 21:30:54 +0100

    Scroll top mobile

--- src/components/ScrollTopButton.tsx
+++ src/components/ScrollTopButton.tsx
@@ -8,7 +8,7 @@ export const ScrollTopButton: React.FC = () => {
   const { scrollPosition } = useContext(GlobalContext);
   return (
     <button
-      className={`fixed top-16 left-16 border-black border-[1px] radius-xl flex justify-center items-center h-8 w-8 transition-opacity transition-duration-500 ${
+      className={`fixed top-8 md:top-16 left-8 md:left-16 border-black border-[1px] radius-xl flex justify-center items-center h-8 w-8 transition-opacity transition-duration-500 ${
         scrollPosition < 20 ? "opacity-0 pointer-events-none" : "opacity-100"
       }`}
       onClick={() => {
